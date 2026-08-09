efc6332836807b8f629834a14c2b505effb4ea87
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-08-09 22:22:04 +0300

    Align Wall Rush wall size regression test

--- tests/wall-rush.test.js
+++ tests/wall-rush.test.js
@@ -47,8 +47,8 @@ test("board is 9 by 13 and supports one selected action", () => {
     assert.doesNotMatch(source, /setOrientation\(/);
     assert.doesNotMatch(source, />Gorizontal<|>Vertikal</);
     assert.match(css, /aspect-ratio:9\/13/);
-    assert.match(css, /\.wr-wall\.horizontal\{[^}]*height:9px/);
-    assert.match(css, /\.wr-wall\.vertical\{[^}]*width:9px/);
+    assert.match(css, /\.wr-wall\.horizontal\{[^}]*height:11px/);
+    assert.match(css, /\.wr-wall\.vertical\{[^}]*width:11px/);
     assert.match(css, /\.wr-wall-target\.horizontal\{height:24px/);
     assert.match(css, /\.wr-wall-target\.vertical\{width:24px/);
 });
