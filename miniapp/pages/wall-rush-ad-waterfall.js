32286c8cdc4dd6ca886206d2c63f24a9445ae17b
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-08-19 09:29:31 +0300

    Add AdsGram Telega rewarded rotation helper

--- miniapp/pages/wall-rush-ad-waterfall.js
+++ miniapp/pages/wall-rush-ad-waterfall.js
@@ -3,25 +3,23 @@
     if (typeof module !== "undefined" && module.exports) module.exports = api;
     if (root) root.WallRushAdWaterfall = api;
 })(typeof globalThis !== "undefined" ? globalThis : this, () => {
-    const fallbackCodes = new Set([
-        "ADSGRAM_NO_FILL",
-        "ADSGRAM_SDK_UNAVAILABLE",
-        "ADSGRAM_SHOW_FAILED",
-        "ADSGRAM_TIMEOUT",
-    ]);
+    const PROVIDERS = Object.freeze(["ADSGRAM", "TELEGA"]);
 
-    function shouldFallbackToTads(error) {
-        return fallbackCodes.has(String(error?.code || ""));
+    function normalizeProvider(provider) {
+        return String(provider || "").toUpperCase() === "TELEGA" ? "TELEGA" : "ADSGRAM";
     }
 
-    async function run({ showAdsgram, showTads }) {
-        try {
-            return { provider: "ADSGRAM", reward: await showAdsgram() };
-        } catch (error) {
-            if (!shouldFallbackToTads(error)) throw error;
-            return { provider: "TADS", reward: await showTads(error) };
+    function nextProvider(provider) {
+        return normalizeProvider(provider) === "ADSGRAM" ? "TELEGA" : "ADSGRAM";
+    }
+
+    async function run({ provider, showAdsgram, showTelega }) {
+        const selected = normalizeProvider(provider);
+        if (selected === "TELEGA") {
+            return { provider: "TELEGA", reward: await showTelega() };
         }
+        return { provider: "ADSGRAM", reward: await showAdsgram() };
     }
 
-    return Object.freeze({ run, shouldFallbackToTads });
+    return Object.freeze({ PROVIDERS, run, normalizeProvider, nextProvider });
 });
